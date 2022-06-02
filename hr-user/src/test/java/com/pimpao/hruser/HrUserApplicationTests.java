974ef5ea7eb50e82586d0b6195cd58882004a0b0
fabianopimpao <fabiano.pimpao@gmail.com>
2022-06-02 07:40:53 -0300

    Project hr-user created

---
+++ hr-user/src/test/java/com/pimpao/hruser/HrUserApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pimpao.hruser;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HrUserApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
